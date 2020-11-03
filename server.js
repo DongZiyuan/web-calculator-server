4b0993533b5f5381d317db29bdf027596f86be9b
DongZiyuan <tracymyth1994@gmail.com>
2020-11-03 11:43:00 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const app = require('express')();
-<<<<<<< HEAD
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
-=======
 const server = require('https').createServer(app);
 const cors = require('cors');
 const io = require('socket.io')(server);
->>>>>>> 0de988d214ac733d352d1426c417fc12329385dc
 const port = process.env.PORT || 4001;
 const queue = [];
 
@@ -26,10 +23,7 @@ function onConnect(socket) {
 	});
 }
 
-<<<<<<< HEAD
-=======
 app.use(cors());
 io.origins('*:*');
->>>>>>> 0de988d214ac733d352d1426c417fc12329385dc
 io.on('connect', onConnect);
 server.listen(port, () => console.log(`listening on port: ${ port }`));
